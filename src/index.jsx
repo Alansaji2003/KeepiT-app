2f81c18cda42d7990925dcf1b3adfa9d0bec556d
Alan Saji <rockstaralansaji@gmail.com>
2024-03-14 22:44:01 +0530

    local files push

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// go to http://localhost:5173/
